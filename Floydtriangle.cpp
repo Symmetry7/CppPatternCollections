2de743c4dbaea95a2342fb286d00cf7d14b472b9
Vinayak Tiwari <vinayakbhai157@gmail.com>
2024-08-07 11:40:53 +0530

    Floydtriangle.cpp

--- Floydtriangle.cpp
+++ Floydtriangle.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
-int main ()
+int main()
 {
-    int num;
+    int num , n=0;
     cout << "Enter any number here : ";
     cin >> num;
-
+    for (int row = 0; row < num; row++)
+    {
+        for (int col = 0; col <= row; col++)
+        {
+            cout << n + 1 <<  " ";
+            n = n + 1;
+        }
+        cout << endl;
+    }
 }
